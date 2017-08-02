488212b5f102a6ce254d42989e0a842b5b5b0e0b
hohoyha <hohoyha@hotmail.com>
2017-08-02 10:57:11 +0900

    socket.io add

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res){
+  res.render('login', {title:'Login'});
+})
+
+router.post('/login', function(req, res){
+  var nick = req.body.username;
+  var password = req.body.password;
+  res.send( nick + ',' + password );
+});
+
 module.exports = router;
